656b982cd830bcc63227c06bfb0855601acaf4b1
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-04-17 17:59:06 -0400

    Create controller for course details

---
+++ CambrianCourseSelectionApplication/CourseDetailsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CourseDetailsViewController.swift
+//  CambrianCourseSelectionApplication
+//
+//  Created by Deepshika Ghale on 2023-04-17.
+//
+
+import UIKit
+
+class CourseDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var courseName: UILabel!
+    
+    @IBOutlet weak var professorName: UILabel!
+    
+    @IBOutlet weak var courseDescription: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
