e3e01388da28742c78839ac703d7bc43cc93829b
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-04-17 17:52:40 -0400

    Show course list in table view controller

--- CambrianCourseSelectionApplication/CoursesTableViewController.swift
+++ CambrianCourseSelectionApplication/CoursesTableViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class CoursesTableViewController: UITableViewController {
-
+    
+    var courseList = CoursesList()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,55 +20,73 @@ class CoursesTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return courseList.courses.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "courseCell", for: indexPath) as! CoursesTableViewCell
 
-        // Configure the cell...
+         //Configure the cell...
+        let row = indexPath.row
+        
+        cell.courseName.text = courseList.courses[row].title
+        cell.courseCode.text = courseList.courses[row].code
 
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView,
+               heightForRowAt indexPath: IndexPath) -> CGFloat {
+       //set the height of the cell
+       return 80
+    }
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            courseList.delete(at: indexPath)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
-    /*
+    
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+        courseList.move(from: fromIndexPath, to: to)
     }
-    */
 
     /*
     // Override to support conditional rearranging of the table view.
@@ -76,14 +96,15 @@ class CoursesTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
     }
-    */
+    
 
 }
