66c1152705d10764b7f6eb727377282fa21ca0fc
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-04-17 17:27:09 -0400

    Create controller for tableview

---
+++ CambrianCourseSelectionApplication/CoursesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CoursesTableViewCell.swift
+//  CambrianCourseSelectionApplication
+//
+//  Created by Deepshika Ghale on 2023-04-17.
+//
+
+import UIKit
+
+class CoursesTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var courseName: UILabel!
+    
+    @IBOutlet weak var courseCode: UILabel!
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- CambrianCourseSelectionApplication/ViewController.swift
+++ CambrianCourseSelectionApplication/ViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var username: UILabel!
+    @IBAction func viewAllCourses(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func viewMyCourses(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
